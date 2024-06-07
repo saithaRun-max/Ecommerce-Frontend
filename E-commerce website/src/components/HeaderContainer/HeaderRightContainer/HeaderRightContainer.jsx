69b73a1ce6688178bac798e7a4079823ab2c7209
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-06-07 16:31:39 +0530

    updated cart page & Login pages

--- E-commerce website/src/components/HeaderContainer/HeaderRightContainer/HeaderRightContainer.jsx
+++ E-commerce website/src/components/HeaderContainer/HeaderRightContainer/HeaderRightContainer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Cart from "./Cart";
-import Login from "./Login";
+import CartBtn from "./CartBtn";
+import LoginBtn from "./LoginBtn";
 
 const HeaderRightContainer = () => {
   return (
     <div className="col-span-2 flex mr-3  justify-between align-middle">
-      <Login />
-      <Cart />
+      <LoginBtn />
+      <CartBtn />
       
     </div>
   );
